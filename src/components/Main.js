87095ef2662b1815f1c1754ceaeb94d52a242ba1
cheonHwi <hoba0409@naver.com>
2022-11-03 15:23:38 +0900

    inital commit

---
+++ src/components/Main.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import {
+  VscFile,
+  VscSearch,
+  VscSourceControl,
+  VscDebugAlt,
+  VscExtensions,
+} from "react-icons/vsc";
+
+const listArr = [
+  {
+    icon: <VscFile size={22.4} />,
+    path: "post",
+  },
+  {
+    icon: <VscSearch size={22.4} />,
+    path: "test",
+  },
+  {
+    icon: <VscSourceControl size={22.4} />,
+    path: "test",
+  },
+  {
+    icon: <VscDebugAlt size={22.4} />,
+    path: "test",
+  },
+  {
+    icon: <VscExtensions size={22.4} />,
+    path: "test",
+  },
+];
+
+function Main() {
+  const [selected, setSelected] = useState(null);
+  return (
+    <Wrap>
+      <LeftBar>
+        {listArr.map((one, index) => (
+          <IconWrap
+            selected={selected === index}
+            onClick={() => {
+              setSelected(index);
+            }}
+          >
+            {one.icon}
+          </IconWrap>
+        ))}
+      </LeftBar>
+    </Wrap>
+  );
+}
+
+export default Main;
+
+const Wrap = styled.div`
+  height: 100vh;
+  background-color: #1e1e1e;
+`;
+
+const LeftBar = styled.div`
+  width: 3rem;
+  height: 100%;
+  background-color: #333333;
+`;
+
+const IconWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 15px 0 15px 0;
+  cursor: pointer;
+
+  > svg {
+    color: ${({ selected }) => (selected ? "#FFF" : "#7a7a7a")};
+  }
+`;
