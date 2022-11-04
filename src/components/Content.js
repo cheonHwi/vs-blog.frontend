949acbcae80d6c008bee75dc3d0d1c229637eb17
cheonHwi <hoba0409@naver.com>
2022-11-04 13:33:47 +0900

    separate : main.js -> this file

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+import Accordion from "./Accordion";
+
+function Content({ type, title, children }) {
+  return type === "directory" ? (
+    <Accordion title={`📂${title}`}>
+      {children?.map((one) => (
+        <Content {...one} />
+      ))}
+    </Accordion>
+  ) : (
+    <div>&nbsp;&nbsp;&nbsp;&nbsp;📝{title}</div>
+  );
+}
+
+export default Content;
