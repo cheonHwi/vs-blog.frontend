50db41e715fbba27198f22032d446b2c09eceb29
cheonHwi <hoba0409@naver.com>
2022-11-04 13:34:28 +0900

    create : list style component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { VscChevronRight, VscChevronDown } from "react-icons/vsc";
 
-function Accordion() {
-  return <div>Accordion</div>;
+// 객체형 파라미터 구조분해할당
+function Accordion({ title, children, isBold }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <>
+      <AccordionWrap
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        {expanded ? (
+          <VscChevronDown size={16} />
+        ) : (
+          <VscChevronRight size={16} />
+        )}
+        <span>{isBold ? <strong>{title}</strong> : title}</span>
+      </AccordionWrap>
+      {expanded && <AccordionContentWrap>{children}</AccordionContentWrap>}
+    </>
+  );
 }
 
 export default Accordion;
+
+const AccordionWrap = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  /* font-weight: bold; */
+  font-size: 0.8rem;
+  padding: 5px 0;
+  cursor: pointer;
+
+  > span {
+    padding-left: 5px;
+    user-select: none;
+  }
+`;
+
+const AccordionContentWrap = styled.div`
+  padding-bottom: 5px;
+  padding-left: 15px;
+`;
